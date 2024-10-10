ae1d481eb23f9349d4158bbd45b46c88f9bd3d28
Abdullah Adel Firihy <144290641+firihy96@users.noreply.github.com>
2024-10-10 08:41:49 +0300

    adding await keyword to users

--- fetch-data.js
+++ fetch-data.js
@@ -3,7 +3,7 @@ let apiUrl ='https://jsonplaceholder.typicode.com/users',
   dataContainer = document.getElementById('api-data');
   try {
     let response = await fetch(apiUrl);
-    let users = ressponse.json();
+    let users = await ressponse.json();
     dataContainer.innerHTML = '';
     let userList = `<ul></ul>`;
     users.forEach(user=> {
