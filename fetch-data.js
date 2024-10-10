3763812d6f0fd6a00cc642b628dde644f490743b
Abdullah Adel Firihy <144290641+firihy96@users.noreply.github.com>
2024-10-10 08:37:47 +0300

    Create fetch-data.js

---
+++ fetch-data.js
@@ -0,0 +1,20 @@
+async function fetchUserData(){
+let apiUrl ='https://jsonplaceholder.typicode.com/users',
+  dataContainer = document.getElementByID('api-data');
+  try {
+    let response = await fetch(apiUrl);
+    let users = ressponse.json();
+    dataContainer.innerHTML = '';
+    let userList = `<ul></ul>`;
+    users.forEach(user=> {
+        let userElement =  `<li>${user}</li>` ;
+      return userList.appendChild(userElement)
+    })
+    dataContainer.appendChild(userList)
+  } catch(error){
+    dataContainer.innerHTML = 'Failed to load user data.';
+  }
+}
+document.addEventListener('DOMContentLoaded', ()=>{
+  fetchUserData();
+})
